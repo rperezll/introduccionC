5859e282efca4d42df721c382d8a0729cf8bfdd9
WINROB\Winrob <robertoprz898@gmail.com>
2017-06-09 18:38:32 +0200

    Actualizado productor-consumidor Threads

--- Threads/productorConsumidorThreads.c
+++ Threads/productorConsumidorThreads.c
@@ -4,7 +4,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <time.h>
 #include <string.h>
 #include <pthread.h>
 #include <semaphore.h>
@@ -14,27 +13,30 @@ FILE * fichero_salida;
 FILE * compartido;
 char buf [1024];
 char buf2 [1024];
-//char cadena [4096];
 int i;
 sem_t sem;
 
-void *th_consumidor (){
-	printf("Soy el consumidor");
+void *th_productor (){
+	printf("Soy el productor\n");
+	int j = 0;
+	compartido = fopen("compartido","w");
 	while (fgets(buf,1024,fichero_numeros)!=NULL){
 		fputs(buf,compartido);
-		sem_post(&sem);
 	}
+	fclose(compartido);
 	sem_post(&sem);
 	pthread_exit(0);
 }
-void *th_productor (){
-	printf("Soy el productor");
+void *th_consumidor (){
+	printf("Soy el consumidor\n");
 	sem_wait(&sem);
-	while(fgets(buf2,1024,compartido)!=NULL){
-		char * b = buf;
+	compartido = fopen("compartido","r");
+	while(fgets(buf2,1024,compartido)!=NULL){	
+		char * b = buf2;
 		i++;
+		char iaux = (char)i;
 		if (*(int *)b % 2 == 0){
-			fput(buf,fichero_salida);			
+			fputs(buf2,fichero_salida);			
 		}	
 	}
 	pthread_exit(0);
@@ -46,10 +48,10 @@ int main (int argc, char *argv[]){
 	fichero_numeros = fopen(argv[1],"r");
 	pthread_t th, th2;
 	sem_init(&sem, 0, 0);
-	pthread_create(&th,NULL,th_consumidor,NULL);
 	pthread_create(&th2,NULL,th_productor,NULL);
-	pthread_join(th_consumidor,NULL);
-	pthread_join(th_productor,NULL);
+	pthread_create(&th,NULL,th_consumidor,NULL);
+	pthread_join(th2,NULL);
+	pthread_join(th,NULL);
 
 }
 
